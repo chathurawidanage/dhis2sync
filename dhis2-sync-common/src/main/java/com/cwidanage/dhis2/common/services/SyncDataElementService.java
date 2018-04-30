dcac22e4bf0730c2a083a9aea685abab2b49e099
Chathura Widanage <chathurawidanage@gmail.com>
2018-04-30 20:54:22 +0530

    Added data validation for SynCDataElement Service

--- dhis2-sync-common/src/main/java/com/cwidanage/dhis2/common/services/SyncDataElementService.java
+++ dhis2-sync-common/src/main/java/com/cwidanage/dhis2/common/services/SyncDataElementService.java
@@ -1,9 +1,11 @@
 package com.cwidanage.dhis2.common.services;
 
+import com.cwidanage.dhis2.common.exceptions.ValidationException;
 import com.cwidanage.dhis2.common.models.sync.SyncDataElement;
 import com.cwidanage.dhis2.common.repositories.SyncDataElementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 /**
  * @author Chathura Widanage
@@ -15,6 +17,11 @@ public class SyncDataElementService {
     private SyncDataElementRepository repository;
 
     public SyncDataElement save(SyncDataElement syncDataElement) {
+        if (StringUtils.isEmpty(syncDataElement.getDisplayName())) {
+            throw new ValidationException("Display name can't be empty");
+        } else if (StringUtils.isEmpty(syncDataElement.getCode())) {
+            throw new ValidationException("Code can't be empty");
+        }
         return this.repository.save(syncDataElement);
     }
 
